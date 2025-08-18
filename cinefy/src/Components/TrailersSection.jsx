950752138705cf7e0d88f49bbadc0c4b54120ab4
ShafinTurjo <shafin.cse.20230104132@aust.edu>
2025-08-19 05:10:54 +0600

    initial

--- cinefy/src/Components/TrailersSection.jsx
+++ cinefy/src/Components/TrailersSection.jsx
@@ -4,7 +4,8 @@ import ReactPlayer from 'react-player';
 import BlurCircle from './BlurCircle';
 
 const TrailersSection = () => {
-    const [currentTrailer, setCurrentTrailer ] = useState(dummyTrailers[0])
+    const [currentTrailer, setCurrentTrailer ] = useState(dummyTrailers && dummyTrailers.length > 0 ? dummyTrailers[0] : null);
+   console.log(currentTrailer?.videoUrl);
 
   return (
     <div className='relative px-6 md:px-16 lg:px-24 xl:px-44 py-20 overflow-hidden'>
@@ -14,10 +15,13 @@ const TrailersSection = () => {
         <div className='relative mt-6'>
             <BlurCircle top='-100px' right = '-100px'/>
              <BlurCircle top="-120px" left="-90px" />
-
-            <ReactPlayer url="https://www.youtube.com/embed/Pez37vmUaQM"
+             {currentTrailer ? (
+            <ReactPlayer url={currentTrailer.videoUrl}
              controls={true} playing={true} muted={true}
             className="mx-auto max-w-full " width="750px" height="500px"/>
+            ) : (
+          <p className="text-center text-red-500">No trailer available</p>
+        )}
         </div>
     </div>
   );
