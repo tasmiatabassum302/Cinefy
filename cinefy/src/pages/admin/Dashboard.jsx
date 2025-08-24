8d40dd91a9ac8c8a49d70e1f36b74fb7ccbff859
tasmiatabassum302 <tasmia.cse.20230104139@aust.edu>
2025-08-25 01:46:01 +0600

    change

--- cinefy/src/pages/admin/Dashboard.jsx
+++ cinefy/src/pages/admin/Dashboard.jsx
@@ -1,9 +1,10 @@
 import React, { useEffect, useState } from 'react';
 import Title from '../../Components/admin/Title';
-import { ChartLineIcon, PlayCircleIcon, UsersIcon } from 'lucide-react';
+import { ChartLineIcon, PlayCircleIcon, StarIcon, UsersIcon } from 'lucide-react';
 import { dummyDashboardData } from '../../assets/assets';
 import Loading from "../../Components/Loading"; 
 import BlurCircle from "../../Components/BlurCircle"; 
+import { dateFormat } from '../../lib/dateFormat';
 
 const Dashboard = () => {
   const [dashboardData, setDashboardData] = useState({
@@ -49,10 +50,30 @@ const Dashboard = () => {
           ))}
         </div>
       </div>
+         <p className="mt-10 text-lg font-medium">Active Shows</p>
+         <div className="relative flex flex-wrap gap-6 mt-4 max-w-5xl"> 
+          <BlurCircle top="100px" left="-10%" />
+          {dashboardData.activeShows.map((show) => ( 
+            <div key={show._id} className="w-55 rounded-lg overflow-hidden h-full pb-3 bg-primary/10 border border-primary/20
+            hover:-translate-y-1 transition duraton-300"> 
+              <img src={show.movie.poster_path} alt='' className="h-60 w-full object-cover"/>
+              <p className="font-medium p-2 truncate">{show.movie.title}</p>
+              <div className="flex items-center justify-between px-2">
+                <p className="text-lg font-medium">Price</p>
+                <p className="flex items-center gap-1 text-sm text-gray-400 mt-1 pr-1">
+                     <StarIcon className="w-4 h-4 text-primary fill-primary" />
+                     {show.movie.vote_average.toFixed(1)}
+                </p>
+
+              </div>
+              <p className="px-2 pt-2 text-sm text-gray-500">{dateFormat(show.showDateTime)}</p>
+          </div>
+        ))}
+
+      </div>
     </>
-  ) : (
-    <Loading />
-  );
-};
+  ) : <Loading />
+  
+}
 
 export default Dashboard;
