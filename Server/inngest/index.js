29380f7ea5e681930fed4ab18b3029dd5cd4c1b3
ShafinTurjo <shafin.cse.20230104132@aust.edu>
2025-08-28 09:46:00 +0600

    sync fixing on index

--- Server/inngest/index.js
+++ Server/inngest/index.js
@@ -7,7 +7,7 @@ export const inngest = new Inngest({ id: "movie-tickect-booking" });
 // to save user data in database
 const syncUserCreation=inngest.createFunction(
     {id: 'sync-user-from-clerk'},
-    {event : 'clerk/user.created'},
+    {event : "clerk/user.created"},
     async({event})=>{
         const {id,first_name,last_name,email_addresses,image_url} =event.data
         const userData={
@@ -22,7 +22,7 @@ const syncUserCreation=inngest.createFunction(
 )
 const syncUserUpdation=inngest.createFunction(
     {id: 'update-user-from-clerk'},
-    {event : 'clerk/user.updated'},
+    {event : "clerk/user.updated"},
     async({event})=>{
         const {id,first_name,last_name,email_addresses,image_url} =event.data
         const userData={
@@ -37,7 +37,7 @@ const syncUserUpdation=inngest.createFunction(
 )
 const syncUserDeletion=inngest.createFunction(
     {id: 'delete-user-with-clerk'},
-    {event : 'clerk/user.deleted'},
+    { event: "clerk/user.deleted" },
     async({event})=>{
         const {id}=event.data
         await User.findByIdAndDelete(id)
