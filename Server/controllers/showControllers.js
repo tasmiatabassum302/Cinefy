c465a1c82eac57cee6d831311622973f18e57e46
ShafinTurjo <shafin.cse.20230104132@aust.edu>
2025-08-31 02:31:16 +0600

    Update showControllers.js

--- Server/controllers/showControllers.js
+++ Server/controllers/showControllers.js
@@ -1,7 +1,14 @@
 export const getNowPlayingMovies= aync(requestAnimationFrame,res) => {
     try{
 
+        const {data}=await axios.get('https://api.themoviedb.org/3/movie/now_playing',{
+            headers:{Authorization:`Bearer ${process.env.TMDB_API_KEY} `}
+        })
+        const movies=data.results;
+        res.json({success:true, movies=movies})
     }catch(error){
+        console.error(error);
+        res.json({success:false, message=error.message})
 
     }
 }
